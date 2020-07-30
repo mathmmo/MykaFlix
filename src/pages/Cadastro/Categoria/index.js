f132e65db6c46d163097f3454ad4e2bb6ae89a13
mathmmo <matheusm.oliveira1995@gmail.com>
2020-07-29 22:56:44 -0300

    Fim dia 02

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import PageDefault from '../../../components/pageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
-const CadastroCategoria = () => (
+function CadastroCategoria() {
+  return (
     <PageDefault>
-        <h1>Cadastro de Categorias</h1>
-        <Link to='/'>Home   </Link>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
     </PageDefault>
-);
+  )
+}
 
 export default CadastroCategoria;
